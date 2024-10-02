5a53277de519d380ab1eb62a218a71f64dc04cdb
Benoit Podwinski <me@benoitpodwinski.com>
2024-10-02 10:03:51 +0200

    Rework db context

--- P7CreateRestApi/Data/LocalDbContext.cs
+++ P7CreateRestApi/Data/LocalDbContext.cs
@@ -1,9 +1,11 @@
 using Microsoft.EntityFrameworkCore;
-using Dot.Net.WebApi.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using P7CreateRestApi.Domain;
 
-namespace Dot.Net.WebApi.Data
+namespace P7CreateRestApi.Data
 {
-    public class LocalDbContext : DbContext
+    public class LocalDbContext : IdentityDbContext<User, IdentityRole<int>, int>
     {
         public LocalDbContext(DbContextOptions<LocalDbContext> options) : base(options) { }
 
@@ -12,7 +14,10 @@ namespace Dot.Net.WebApi.Data
             base.OnModelCreating(builder);
         }
 
-        public DbSet<User> Users { get; set;}
         public DbSet<BidList> Bids { get; set; }
+        public DbSet<CurvePoint> CurvePoints { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<RuleName> RuleNames { get; set; }
+        public DbSet<Trade> Trades { get; set; }
     }
 }
