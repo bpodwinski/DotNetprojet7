3742fe44b0c8e1ef9b12fc3e9c368ed6160f73ed
Benoit Podwinski <me@benoitpodwinski.com>
2024-10-02 10:03:16 +0200

    Fix DTOs

--- P7CreateRestApi/DTOs/UserDTO.cs
+++ P7CreateRestApi/DTOs/UserDTO.cs
@@ -3,7 +3,7 @@
     public class UserDTO
     {
         public int Id { get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public string Password { get; set; }
         public string FullName { get; set; }
         public string Role { get; set; }
